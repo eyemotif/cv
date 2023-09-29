4717b8d4258e5d432abdbf6dcbc7173639cdd975
eyemotif <yepicness@gmail.com>
2023-09-29 14:31:17 -0400

    Add short'n'sweet bio to the About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
-    <p>This is an about view!</p>
-    <p>I'm gonna type something here!</p>
+    <h1>I'm an artist.</h1>
+    <p>
+        I love making art of all kinds, whether it be websites, RESTful APIs,
+        music, or even fictional languages.
+    </p>
+    <p>
+        I also think creating art with others is a beautiful thing. I love
+        collaborating and working in teams!
+    </p>
+    <p>
+        I'm passionate about everything I make and work on, and I'm always
+        searching for new ways to make more art.
+    </p>
 </template>
